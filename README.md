8fe934c2f29aede65ca213bd0e2a7f9a1814ee90
Vincent Clemson <vclemson07@gmail.com>
2019-08-17 17:23:01 -0400

    Create a time warp into the 4th dimension to allow for parallel develop on css git submodule & xaringan webpage page ... ninja

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The [Xaringan](https://github.com/yihui/xaringan) slide show template for the Mo
 ## Description
 This contains a base R Markdown file to create the [Xaringan](https://github.com/yihui/xaringan) static content website presentation, a build script, and all other static content (images & JavaScript library specific R Markdown Xaringan extension libraries). 
 
-In addition, this template contains a git submodule, which contains the [Xaringan theme](https://github.com/yihui/xaringan/wiki/Themes) for the mod-mAndA series, which can be found at [mAndA/mod-mAndA-theme](https://esgovcloud.com/mAndA/mod-mAndA-theme).
+In addition, this template contains a [git submodule](https://git-scm.com/docs/gitsubmodules), which contains the [Xaringan theme](https://github.com/yihui/xaringan/wiki/Themes) for the mod-mAndA series, which can be found at [mAndA/mod-mAndA-theme](https://esgovcloud.com/mAndA/mod-mAndA-theme).
 
 ## Clone
 
--- build.R
+++ build.R
@@ -1,1 +1,34 @@
-xaringan:::inf_mr()
+# Starting from scratch & Build
+#
+#`git clone --recursive git@esgovcloud.com:mAndA/mod-mAndA-template.git`
+#`git remote add origin git@url`
+#make changes
+xaringan::inf_mr()
+#add & commit changes
+#`git push -u origin master`
+
+# Building website & Deploy
+#
+# This allows us to source the filesystem in the main superproject when in the remote
+# repo. The magic is all in build command below AND the index.Rmd YALM!
+# (multiple stylesheet paths) 
+fs::dir_copy(path = 'theme/src', 'dist/src')
+fs::dir_copy(path = 'theme/usr', 'dist/usr')
+xaringan::moon_reader()
+#`git push`
+
+# Git Submodule workflow
+#
+#
+
+#Unfortunately, we cannot set the `extra_dependencies` argument in xaringan because
+#it utilizes the arguments of BOTH rmarkdown::html_document() & html_document_base()
+#which BOTH contain `extra_dependencies`, so we get a 
+#"matched by multiple actual arguments" error
+#xaringan::moon_reader()rmarkdown::html_document()rmarkdown::html_document_base()
+
+# Making updates to the theme should be done DIRECTLY to the theme directory 
+# i.e. the git submodule
+
+# setup GitHub Pages enterprise directory structure
+#`currently NOTHING woop!`
